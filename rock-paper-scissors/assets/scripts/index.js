4e95d1fa07342edc51cb9d1b30518058a38be6a8
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-05-17 08:52:55 -0400

    Add rock-paper-scissors and static-job-listings solutions

---
+++ rock-paper-scissors/assets/scripts/index.js
@@ -0,0 +1,138 @@
+const selectableHandsContainer = document.querySelector('.selectable-hands');
+const selectableHands = selectableHandsContainer.querySelectorAll('.hand');
+const handNames = Array.from(selectableHands).map((hand) => hand.getAttribute('data-hand'));
+
+selectableHands.forEach((hand) => {
+  hand.addEventListener('click', (clickEvent) => {
+    onHandClicked(clickEvent.target);
+  });
+  hand.addEventListener('keyup', (keyEvent) => {
+    if (keyEvent.key === 'Enter') {
+      onHandClicked(keyEvent.target);
+    }
+  });
+});
+
+const playedHandsContainer = document.querySelector('.played-hands');
+const playerHandElement = document.querySelector('#player-hand');
+const playerPickedText = document.querySelector('#player-hand + .picked-text');
+const houseHandElement = document.querySelector('#house-hand');
+const housePickedText = document.querySelector('#house-hand + .picked-text');
+
+const resultElement = document.querySelector('.result');
+const resultText = document.getElementById('result-text');
+
+const playAgainButton = document.getElementById('play-again');
+playAgainButton.addEventListener('click', startNewRound);
+
+function startNewRound() {
+  toggleVisibilityOf(selectableHandsContainer);
+  toggleVisibilityOf(playedHandsContainer);
+  toggleVisibilityOf(resultElement);
+
+  // clear these for the next round
+  showPlayerHand('placeholder');
+  showHouseHand('placeholder');
+}
+
+// .hidden applies display: none
+function toggleVisibilityOf(element) {
+  element.classList.toggle('hidden');
+}
+
+function pickRandomHouseHand() {
+  return handNames[Math.floor(Math.random() * handNames.length)];
+}
+
+const handBeatenBy = {
+  rock: 'scissors',
+  scissors: 'paper',
+  paper: 'rock',
+};
+
+function getResult(playerHand, houseHand) {
+  if (playerHand === houseHand) {
+    return { scoreChange: 0, winner: null, feedback: 'TIE GAME' };
+  }
+
+  if (handBeatenBy[playerHand] === houseHand) {
+    return { scoreChange: 1, winner: 'player-hand', feedback: 'YOU WIN' };
+  }
+
+  return { scoreChange: -1, winner: 'house-hand', feedback: 'YOU LOSE' };
+}
+
+function onHandClicked(hand) {
+  const playerHand = hand.getAttribute('data-hand');
+  const houseHand = pickRandomHouseHand();
+  evaluateRoundResult(playerHand, houseHand);
+}
+
+function showPlayerHand(playerHand) {
+  playerHandElement.className = `hand ${playerHand}`;
+  playerPickedText.setAttribute('aria-label', `You picked ${playerHand}`);
+}
+
+function showHouseHand(houseHand) {
+  houseHandElement.className = `hand ${houseHand}`;
+  housePickedText.setAttribute('aria-label', `The house picked ${houseHand}`);
+}
+
+const scoreElement = document.getElementById('score');
+function changeScoreBy(value) {
+  scoreElement.innerHTML = Number(scoreElement.innerHTML) + value;
+}
+
+function highlightWinningHand(winner) {
+  document.getElementById(winner).classList.add('winning-hand');
+}
+
+function evaluateRoundResult(playerHand, houseHand) {
+  toggleVisibilityOf(selectableHandsContainer);
+  toggleVisibilityOf(playedHandsContainer);
+
+  setTimeout(() => showPlayerHand(playerHand), 500);
+  setTimeout(() => showHouseHand(houseHand), 1000);
+  setTimeout(() => {
+    toggleVisibilityOf(resultElement);
+    const result = getResult(playerHand, houseHand);
+    resultText.innerHTML = result.feedback;
+    changeScoreBy(result.scoreChange);
+
+    // Anything but a tie
+    if (result.winner) {
+      highlightWinningHand(result.winner);
+    }
+  }, 1500);
+}
+
+const rulesModalWrapper = document.querySelector('.rules .modal-wrapper');
+const rulesModalWindow = rulesModalWrapper.querySelector('.modal-window');
+const closeModalButton = document.querySelector('#close-modal-button');
+const rulesButton = document.getElementById('rules-button');
+
+function openRulesModal() {
+  rulesModalWrapper.classList.add('opened');
+}
+
+function closeRulesModal() {
+  rulesModalWrapper.classList.remove('opened');
+}
+
+document.addEventListener('keyup', (keyEvent) => {
+  if (keyEvent.key === 'Escape') {
+    // doesn't matter if it's not open because the classlist just won't remove anything
+    closeRulesModal();
+  }
+});
+
+rulesModalWindow.addEventListener('click', (clickEvent) => {
+  clickEvent.stopPropagation();
+});
+
+rulesModalWrapper.addEventListener('click', () => {
+  closeRulesModal();
+});
+
+rulesButton.addEventListener('click', openRulesModal);
+closeModalButton.addEventListener('click', closeRulesModal);
---
+++ static-job-listings/assets/scripts/index.js
@@ -0,0 +1,309 @@
+// jobListings will never be mutated after initial assignment
+let jobListings = [];
+const jobListingsElement = document.getElementById("job-listings");
+
+// Using a set to store unique filters as strings
+const activeFilters = new Set();
+const filtersWrapper = document.getElementById("filters");
+const activeFiltersElement = filtersWrapper.querySelector(".active-filters");
+
+function getDataFromAPI() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          company: "Photosnap",
+          logo: "./assets/img/photosnap.svg",
+          new: true,
+          featured: true,
+          position: "Senior Frontend Developer",
+          role: "Frontend",
+          level: "Senior",
+          postedAt: "1d ago",
+          contract: "Full Time",
+          location: "USA Only",
+          languages: ["HTML", "CSS", "JavaScript"]
+        },
+        {
+          id: 2,
+          company: "Manage",
+          logo: "./assets/img/manage.svg",
+          new: true,
+          featured: true,
+          position: "Fullstack Developer",
+          role: "Fullstack",
+          level: "Midweight",
+          postedAt: "1d ago",
+          contract: "Part Time",
+          location: "Remote",
+          languages: ["Python"],
+          tools: ["React"]
+        },
+        {
+          id: 3,
+          company: "Account",
+          logo: "./assets/img/account.svg",
+          new: true,
+          featured: false,
+          position: "Junior Frontend Developer",
+          role: "Frontend",
+          level: "Junior",
+          postedAt: "2d ago",
+          contract: "Part Time",
+          location: "USA Only",
+          languages: ["JavaScript"],
+          tools: ["React", "Sass"]
+        },
+        {
+          id: 4,
+          company: "MyHome",
+          logo: "./assets/img/myhome.svg",
+          new: false,
+          featured: false,
+          position: "Junior Frontend Developer",
+          role: "Frontend",
+          level: "Junior",
+          postedAt: "5d ago",
+          contract: "Contract",
+          location: "USA Only",
+          languages: ["CSS", "JavaScript"]
+        },
+        {
+          id: 5,
+          company: "Loop Studios",
+          logo: "./assets/img/loop-studios.svg",
+          new: false,
+          featured: false,
+          position: "Software Engineer",
+          role: "FullStack",
+          level: "Midweight",
+          postedAt: "1w ago",
+          contract: "Full Time",
+          location: "Worldwide",
+          languages: ["JavaScript"],
+          tools: ["Ruby", "Sass"]
+        },
+        {
+          id: 6,
+          company: "FaceIt",
+          logo: "./assets/img/faceit.svg",
+          new: false,
+          featured: false,
+          position: "Junior Backend Developer",
+          role: "Backend",
+          level: "Junior",
+          postedAt: "2w ago",
+          contract: "Full Time",
+          location: "UK Only",
+          tools: ["RoR"]
+        },
+        {
+          id: 7,
+          company: "Shortly",
+          logo: "./assets/img/shortly.svg",
+          new: false,
+          featured: false,
+          position: "Junior Developer",
+          role: "Frontend",
+          level: "Junior",
+          postedAt: "2w ago",
+          contract: "Full Time",
+          location: "Worldwide",
+          languages: ["HTML", "JavaScript"],
+          tools: ["Sass"]
+        },
+        {
+          id: 8,
+          company: "Insure",
+          logo: "./assets/img/insure.svg",
+          new: false,
+          featured: false,
+          position: "Junior Frontend Developer",
+          role: "Frontend",
+          level: "Junior",
+          postedAt: "2w ago",
+          contract: "Full Time",
+          location: "USA Only",
+          languages: ["JavaScript"],
+          tools: ["Vue", "Sass"]
+        },
+        {
+          id: 9,
+          company: "Eyecam Co.",
+          logo: "./assets/img/eyecam-co.svg",
+          new: false,
+          featured: false,
+          position: "Full Stack Engineer",
+          role: "Fullstack",
+          level: "Midweight",
+          postedAt: "3w ago",
+          contract: "Full Time",
+          location: "Worldwide",
+          languages: ["JavaScript", "Python"],
+          tools: ["Django"]
+        },
+        {
+          id: 10,
+          company: "The Air Filter Company",
+          logo: "./assets/img/the-air-filter-company.svg",
+          new: false,
+          featured: false,
+          position: "Front-end Dev",
+          role: "Frontend",
+          level: "Junior",
+          postedAt: "1mo ago",
+          contract: "Part Time",
+          location: "Worldwide",
+          languages: ["JavaScript"],
+          tools: ["React", "Sass"]
+        }
+      ]);
+    }, 1000);
+  });
+}
+
+function renderListings() {
+  jobListingsElement.innerHTML = jobListings
+    .map(
+      listing =>
+        `<div id="listing-${listing.id}" class="job-listing card${
+          listing.featured ? " featured" : ""
+        }" data-tags="${listing.tags.join(" ")}">
+      <header class="listing-header">
+        <img src="${listing.logo}" alt="${
+          listing.company
+        }'s logo" class="company-logo" />
+        <div>
+          <div class="listing-details">
+            <span class="company-name">${listing.company}</span>
+            <ul class="listing-types">
+                ${
+                  listing.new
+                    ? `<li class="new listing-type pill">NEW!</li>`
+                    : ""
+                }
+                ${
+                  listing.featured
+                    ? `<li class="featured listing-type pill">FEATURED</li>`
+                    : ""
+                }
+            </ul>
+          </div>
+          <div class="position">
+            <div class="title">${listing.position}</div>
+            <div class="details">
+              <div class="detail posted-at">${listing.postedAt}</div>
+              <div class="detail contract">${listing.contract}</div>
+              <div class="detail location">${listing.location}</div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <hr>
+      <footer class="tags">
+      ${listing.tags.map(tag => `<a class="tag" href="#0">${tag}</a>`).join("")}
+      </footer>
+    </div>`
+    )
+    .join("");
+
+  document.querySelectorAll(".tag").forEach(tag => {
+    tag.addEventListener("click", clickEvent => {
+      addFilter(clickEvent.target.innerText);
+    });
+  });
+}
+
+function addFilter(filter) {
+  if (activeFilters.has(filter)) return;
+
+  // If we're adding the very first filter, set the wrapper to be visible
+  if (!activeFilters.size) {
+    filtersWrapper.classList.add("visible");
+  }
+
+  activeFilters.add(filter);
+
+  const newFilter = document.createElement("div");
+  newFilter.classList.add("filter");
+  newFilter.id = `filter-${filter}`;
+
+  const filterName = document.createElement("span");
+  filterName.classList.add("filter-name");
+  filterName.innerText = filter;
+
+  const clearFilterButton = document.createElement("a");
+  clearFilterButton.setAttribute("href", "#0");
+  clearFilterButton.classList.add("clear-filter");
+  clearFilterButton.addEventListener("click", clickEvent => {
+    removeFilter(clickEvent.target.parentNode);
+  });
+
+  newFilter.appendChild(filterName);
+  newFilter.appendChild(clearFilterButton);
+  activeFiltersElement.appendChild(newFilter);
+  filterListings();
+}
+
+function removeFilter(filter) {
+  activeFiltersElement.removeChild(filter);
+  activeFilters.delete(filter.innerText);
+  filterListings();
+  if (!activeFilters.size) {
+    filtersWrapper.classList.remove("visible");
+  }
+}
+
+function clearAllFilters() {
+  activeFilters.clear();
+  activeFiltersElement.innerHTML = "";
+  filtersWrapper.classList.remove("visible");
+  filterListings();
+}
+
+function filterListings() {
+  // Make all listings visible
+  if (activeFilters.size === 0) {
+    document
+      .querySelectorAll(".job-listing")
+      .forEach(listing => (listing.style.display = "flex"));
+    return;
+  }
+
+  jobListings.forEach(listing => {
+    const listingElement = document.getElementById(`listing-${listing.id}`);
+    // If every filter is present in a listing's tags, then show that listing (flex)
+    if (Array.from(activeFilters).every(tag => listing.tags.includes(tag))) {
+      listingElement.style.display = "flex";
+    } else {
+      listingElement.style.display = "none";
+    }
+  });
+}
+
+getDataFromAPI().then(data => {
+  jobListings = data.map(listing => {
+    // ES6 destructuring assignment, to avoid storing duplicate data (see return statement)
+    const { tools, languages, role, level, ...listingData } = listing;
+
+    const tags = [role, level];
+
+    if (listing.languages) {
+      listing.languages.forEach(lang => tags.push(lang));
+    }
+
+    if (listing.tools) {
+      listing.tools.forEach(tool => tags.push(tool));
+    }
+
+    // Here. If we didn't destructure, we'd have unnecessary, duplicate data
+    // stored across level, role, tools, languages, and again in tags.
+    return { ...listingData, tags };
+  });
+  renderListings();
+});
+
+document
+  .getElementById("clear-filters")
+  .addEventListener("click", clearAllFilters);
